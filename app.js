026f9e36fe455b2f0aae70e7c72c35f5ceb35215
RandomSeeded <nate.a.willard@gmail.com>
2017-11-12 09:35:34 -0800

    working bidirectional message sending

--- app.js
+++ app.js
@@ -20,6 +20,46 @@ const headers = {
   'User-agent': 'Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)',
 };
 
+let tinderSelfId;
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+app.post('/sms', async (req, res, next) => {
+  const messageBody = req.body.Body;
+  const twiml = new MessagingResponse();
+  twiml.message('');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+
+  const mostRecentMatch = await getMostRecentMatch();
+  sendMessage(mostRecentMatch._id, messageBody);
+});
+
+app.listen(1337, () => console.log('Example app listening on port 1337!'))
+
+async function sendMessage(matchId, message) {
+  console.log('sending tinder message', message);
+  const url = `${host}/user/matches/${matchId}`;
+  const requestOpts = {
+    url,
+    headers,
+    body: JSON.stringify({ message }),
+  };
+  return new Promise((resolve, reject) => {
+    request.post(requestOpts, function(err, res, body) {
+      if (err) return reject(err);
+
+      resolve();
+    });
+  });
+}
 
 async function getAuthToken(facebook_token, facebook_id, cb) {
   const url = `${host}/auth`;
@@ -32,8 +72,9 @@ async function getAuthToken(facebook_token, facebook_id, cb) {
   return new Promise((resolve, reject) => {
     request.post(requestOpts, function(err, res, body) {
       if (err) return reject(err);
+      const parsedBody = JSON.parse(body);
 
-      resolve(JSON.parse(body).token);
+      resolve([parsedBody.token, parsedBody.user._id]);
     });
   });
 };
@@ -53,6 +94,12 @@ async function getMatches() {
   });
 }
 
+async function getMostRecentMatch() {
+  const matches = await getMatches();
+  return _.first(matches);
+}
+
+
 // Super MVP mode: just check to see if you've received a message in the last 5 minutes
 // And set that equal to the poll time
 // Ya this is lame and not great...oh well
@@ -65,7 +112,7 @@ function checkMatchHasRecentMessage(match) {
 
 const messageCache = {};
 async function getNewMessagesForMatch(match) {
-  const url = `${host}/v2/matches/${match._id}/messages?count=100&locale=en&page_token=MjAxNy0xMS0xMlQwMjo0NToyNC44MzNa`;
+  const url = `${host}/v2/matches/${match._id}/messages?count=100&locale=en`;
   const requestOpts = { url, headers };
   return new Promise((resolve, reject) => {
     request.get(requestOpts, function(err, res, body) {
@@ -76,7 +123,7 @@ async function getNewMessagesForMatch(match) {
       const newMessages = _.filter(parsedBody.data.messages, message => {
         const isNewMessage = !messageCache[message._id];
         messageCache[message._id] = message;
-        return isNewMessage;
+        return isNewMessage && message.from !== tinderSelfId;
       });
       const newMessagesWithName = _.map(newMessages, message => {
         message.name = match.person.name
@@ -89,6 +136,7 @@ async function getNewMessagesForMatch(match) {
 }
 
 async function sendSMS(body) {
+  console.log('sending SMS', body);
   return client.messages
     .create({
       to: TEMP_TOKENS.myPhoneNumber,
@@ -104,18 +152,21 @@ function generateMessageBody(message) {
     ${message.message}`;
 }
 
-async function run() {
-  const authToken = await getAuthToken(facebookAuthToken.getFacebookAccessToken(), facebookAuthToken.getFacebookId());
+async function run(sendMessages) {
+  const [authToken, selfId] = await getAuthToken(facebookAuthToken.getFacebookAccessToken(), facebookAuthToken.getFacebookId());
+  tinderSelfId = selfId;
   headers['X-Auth-Token'] = authToken;
   const matches = await getMatches();
   const peopleWithNewMessages = _.filter(matches, checkMatchHasRecentMessage);
   const newMessages = await Promise.all(_.map(peopleWithNewMessages, getNewMessagesForMatch));
   const formattedMessages = _.map(_.flatten(newMessages), generateMessageBody);
-  // Product question: do we want to include the messages you sent? No.
-  _.each(formattedMessages, sendSMS);
+  // Don't send messages the first time we startup: this is just to populate the cache
+  if (sendMessages) {
+    _.each(formattedMessages, sendSMS);
+  }
 }
 
-run();
+run(false);
 setInterval(() => {
-  run();
+  run(true);
 }, 5000);
